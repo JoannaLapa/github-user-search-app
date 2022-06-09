644baef7cfee9f761b4e00702a0c18764256a2cd
Joanna <jlapa.transport@gmail.com>
2022-06-09 13:52:57 +0200

    changeColorScreenMode function made and put in addEventListener

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,31 @@
-const button = document.querySelector('#screen-mode__button');
-const body = document.querySelector('body');
+const button = document.querySelector('#screen-mode__button')
+const body = document.querySelector('body')
 const headerWrapper = document.querySelector('#headerWrapper')
 let theme = localStorage.getItem('theme') || 'light'
-button.addEventListener("click", () => {
-   if(theme === "dark"){
-    body.classList.remove('dark')
-    button.textContent = 'DARK'
-    theme = 'light'
-   }else {
-    body.classList.add('dark')
-    button.textContent = 'LIGHT'
-    theme = 'dark'
-   }
-localStorage.setItem('theme', theme)
+
+const changeColorScreenMode = () => {
+	if (
+		window.matchMedia('(prefers-color-scheme: dark)').matches === true &&
+		body.classList.contains('light') !== true &&
+		body.classList.contains('dark') !== true
+	) {
+		body.classList.add('light')
+		theme = 'light'
+		console.log(theme)
+	} else if (body.classList.contains('light')) {
+		body.classList.add('dark')
+		body.classList.remove('light')
+		theme = 'dark'
+		console.log(theme)
+	} else {
+		body.classList.remove('dark')
+		body.classList.add('light')
+		theme = 'light'
+		console.log(theme)
+	}
+	localStorage.setItem('theme', theme)
+}
+button.addEventListener('click', () => {
+	changeColorScreenMode()
 })
 
-if(theme === 'dark') {
-    body.classList.add('dark')
-    button.textContent = 'LIGHT'
-}
